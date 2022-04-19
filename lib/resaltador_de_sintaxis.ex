0230ffbd6ae1bc1ff72ebe395d6732794e8b6854
ale_local <alejo.ubeto@gmail.com>
2022-04-18 23:37:52 -0500

    Creación de estructura del proyecto

---
+++ lib/resaltador_de_sintaxis.ex
@@ -0,0 +1,18 @@
+defmodule ResaltadorDeSintaxis do
+  @moduledoc """
+  Documentation for `ResaltadorDeSintaxis`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ResaltadorDeSintaxis.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
