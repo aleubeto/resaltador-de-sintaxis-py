2b7a97c513028aae6f94da266ebc4b86273d708f
ale-local <alejo.ubeto@gmail.com>
2022-06-10 11:36:02 -0500

    Primeras pruebas benchee exitosas

--- lib/benchmark.exs
+++ lib/benchmark.exs
@@ -1,4 +1,8 @@
 Benchee.run(%{
   "secuencial" => fn -> ResaltadorSecuencial.main("test") end,
-  "concurrente" => fn -> ResaltadorConcurrente.concurrencia("test") end
-})
+  "concurrente" => fn -> ResaltadorConcurrente.main("test") end
+  },
+  formatters: [
+    Benchee.Formatters.HTML,
+    Benchee.Formatters.Console
+  ])
