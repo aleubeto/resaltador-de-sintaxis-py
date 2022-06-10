5cb9bf27b7935f08f77f9a68c40d91ef92cb8452
ale-local <alejo.ubeto@gmail.com>
2022-06-09 22:44:33 -0500

    Secuencial con ciclo for

--- lib/resaltador_secuencial.ex
+++ lib/resaltador_secuencial.ex
@@ -2,17 +2,22 @@ defmodule ResaltadorSecuencial do
 
     # Funcion ejecutable del HTML:
     # ResaltadorSecuencial.main(["test/prueba1.py", "index.html"])
-    def main(args\\[]) do
-        lectura = read_file(hd(args))
-        formato = format(elem(lectura,1))
-        File.rm(hd(tl(args)))
-        {:ok, archivo} = File.open(hd(tl(args)), [:write])
-        IO.binwrite(archivo, '<link rel = "stylesheet" href="lib/style.css">')
-        IO.binwrite(archivo, '<body><div class="container"><p>')
-        fin = htmlgen(formato, 1, archivo)
-        IO.binwrite(archivo, '<p/></div><body/>')
-        File.close(archivo)
-        fin
+
+    def main() do
+        test_files = File.ls("test/test_files") |> elem(1) |> Enum.filter(&String.ends_with?(&1, ".py"))
+        for file <- test_files do
+            lectura = read_file("test/test_files/"<>file)
+            formato = format(elem(lectura,1))
+            File.rm(file)
+            {:ok, archivo} = File.open("test/test_output/#{file}.html", [:write])
+            IO.binwrite(archivo, '<title>#{file}</title><link rel="icon" href="../../lib/css/img/python.png"')
+            IO.binwrite(archivo, '<link rel="stylesheet" href="../../lib/css/style.css">')
+            IO.binwrite(archivo, '<body><div class="container"><p>')
+            fin = htmlgen(formato, 1, archivo)
+            IO.binwrite(archivo, '<p/></div><body/>')
+            File.close(archivo)
+            fin
+        end
     end
 
     # Función que procesa el archivo .py
@@ -23,11 +28,11 @@ defmodule ResaltadorSecuencial do
     # Función que genera una línea html como respuesta a un token
     def format(tokens) do
         Enum.map(tokens, fn {token, line, tchars} ->
-        if token == :tab do
-            {line, "&emsp;"}
-        else
-            {line, "<span class=#{token}>#{tchars}</span>"}
-        end
+            if token == :tab do
+                {line, "&emsp;"}
+            else
+                {line, "<span class=#{token}>#{tchars}</span>"}
+            end
         end)
     end
 
@@ -38,12 +43,12 @@ defmodule ResaltadorSecuencial do
     def htmlgen(list, acc, file) do
         c = acc
         if c == elem(hd(list),0) do
-        IO.binwrite(file, elem(hd(list), 1))
-        htmlgen(tl(list), c, file)
+            IO.binwrite(file, elem(hd(list), 1))
+            htmlgen(tl(list), c, file)
         else
-        c = c + 1
-        IO.binwrite(file, "<br>")
-        htmlgen(list,c,file)
+            c = c + 1
+            IO.binwrite(file, "<br>")
+            htmlgen(list,c,file)
         end
     end
 
